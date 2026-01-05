fd24a8ccbbdc6ff8eb26ce4d5c5927c13c96dc6b
Ivan Gomez <ivandavidgomezsilva@hotmail.com>
2026-01-04 20:19:51 -0500

    New backend for handling influencer and job applications.

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1465,6 +1465,37 @@ LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+-- Table structure for table `application_jobs`
+
+DROP TABLE IF EXISTS `application_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `application_jobs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `application_type` varchar(50) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  user_id int DEFAULT NULL,
+  `data` json DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_application_jobs_email` (`email`),
+  KEY `ix_application_jobs_application_type` (`application_type`),
+  KEY `ix_application_jobs_created_at` (`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `application_jobs`
+--
+
+LOCK TABLES `application_jobs` WRITE;
+/*!40000 ALTER TABLE `application_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `application_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
 --
 -- Table structure for table `users`
 --
