8081e78616e6b8c3048c2e82990cc9cdf5d08bd6
Shrey Dikshant <shreydikshant144@example.com>
2026-01-11 02:18:12 +0530

    Blueprint add

--- app/blueprints.py
+++ app/blueprints.py
@@ -41,6 +41,7 @@ from .routes import (
     # background_remover,
     qwen_chat_bp_pdg_br,
     image_editor_routes,
+    business_plan_routes,
     cf_img_proccessing_routes,
     
     permission_routes,
@@ -108,7 +109,8 @@ blueprints = [
 { "blueprint": waitlist_routes.waitlist_bp, "url_prefix": '/api/waitlist'},
 { "blueprint": pdf_signing_routes.pdf_bp, "url_prefix": '/api/pdf'},
 # { "blueprint": background_remover.background_bp, "url_prefix": '/api/background-remover'},
-{ "blueprint": qwen_chat_bp_pdg_br.qwen_bp, "url_prefix": '/api/ai'},
+{ "blueprint": qwen_chat_bp_pdg_br.qwen_bp, "url_prefix": '/api/ai'},\
+{ "blueprint": business_plan_routes.plans_bp, "url_prefix": "/api/plans" },
 { "blueprint": image_editor_routes.image_editor_bp, "url_prefix": '/api/image-editor'},
 { "blueprint": cf_img_proccessing_routes.cf_bp, "url_prefix": '/api/cf'},
 { "blueprint": feedback_routes.feedback_bp, "url_prefix": '/api/feedback' },
